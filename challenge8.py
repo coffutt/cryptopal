e66a1d5100ddf499ed67615318ce1ec35848e8ab
coffutt <coffutt@paypal.com>
2015-02-26 10:43:13 -0500

    Refactored challenge 8

--- challenge8.py
+++ challenge8.py
@@ -6,21 +6,14 @@ def ecb_score(cypher_data):
     key_len = 16
     blocks = [cypher_data[i:i+key_len] for i in range(0, len(cypher_data), key_len)]
     pairs = itertools.combinations(blocks, 2)
-    return sum(map(lambda p: p[0] == p[1], pairs)) / float(len(cypher_data))
+    return sum(map(lambda p: p[0] == p[1], pairs))
 
-def find_ecb_cypher(cyphers):
-    max_score = (0, None)
-
-    for i in range(len(cyphers)):
-        score = ecb_score(cyphers[i])
-        if score > max_score[0]:
-            max_score = (score, i)
-
-    return max_score
+def find_ecb_cyphers(cyphers):
+    t_lines = [(i, ecb_score(cyphers[i-1])) for i in range(1,len(cyphers)+1)]
+    return filter(lambda l: l[1] > 0, t_lines)
 
 if __name__ == '__main__':
     with open('./data/8.txt') as f:
         from binascii import unhexlify
         data = map(lambda l: unhexlify(l.rstrip()), f.readlines())
-
-        print find_ecb_cypher(data)
+        print find_ecb_cyphers(data)
