c0fca1b2c050ac35c895c3f716c4e1cdeda21454
coffutt <coffutt@paypal.com>
2015-02-27 08:16:59 -0500

    Got random encryptor working for challenge 11

--- challenge10.py
+++ challenge10.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from challenge2 import xor
-from challenge7 import aes_ecb_decrypt as ecb_decrypt
+from challenge7 import aes_ecb_decrypt, aes_ecb_encrypt
 
 ascii_zero = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
@@ -14,12 +14,26 @@ def aes_cbc_decrypt(cipher_text, key, iv=ascii_zero):
     last_block = iv
 
     for block in blocks:
-        plain_text += xor(ecb_decrypt(block, key), last_block)
+        plain_text += xor(aes_ecb_decrypt(block, key), last_block)
         last_block = block
 
     return plain_text
 
+def aes_cbc_encrypt(plain_text, key, iv=ascii_zero):
+    blocks = get_blocks(plain_text, len(key))
+    cipher_text = ''
+    last_cipher_block = iv
+
+    for block in blocks:
+        cipher_block = aes_ecb_encrypt(xor(last_cipher_block, block), key)
+        cipher_text += cipher_block
+        last_cipher_block = cipher_block
+
+    return cipher_text
+
 if __name__ == '__main__':
     with open('./data/10.txt') as f:
         import base64
-        print aes_cbc_decrypt(base64.b64decode(f.read()), 'YELLOW SUBMARINE')
+        decrypted = aes_cbc_decrypt(base64.b64decode(f.read()), 'YELLOW SUBMARINE')
+        encrypted = aes_cbc_encrypt(decrypted, 'YELLOW SUBMARINE')
+        print base64.b64encode(encrypted)
--- challenge11.py
+++ challenge11.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 
 import os
+from random import randint, getrandbits as randbits
+from challenge7 import aes_ecb_encrypt
+from challenge10 import aes_cbc_encrypt
+from challenge9 import pkcs7_pad
 
-def random_aes_key(length=16):
+def rand_bytes(length=16):
     return os.urandom(length)
 
+def pad(data):
+    return pkcs7_pad(rand_bytes(randint(5,10)) + data + rand_bytes(randint(5,10)))
+
+def encrypt_random(data):
+    key = rand_bytes()
+    padded = pad(data)
+
+    if bool(randbits(1)):
+        return aes_ecb_encrypt(padded, key)
+    else:
+        return aes_cbc_encrypt(padded, key, rand_bytes())
+
+def encryption_orcale(cipher_text):
+
+
 if __name__ == '__main__':
-    print random_aes_key()
+    print encrypt_random('Some data')
