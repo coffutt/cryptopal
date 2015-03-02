f25fc85fa081bab99c3cb110a17d21cdedf95d72
coffutt <coffutt@paypal.com>
2015-03-02 14:59:01 -0500

    Challenge 16

--- challenge16.py
+++ challenge16.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+    #!/usr/bin/python
 
 from string import split as str_split
 from challenge9 import pkcs7_pad
@@ -6,10 +6,13 @@ from challenge10 import aes_cbc_encrypt, aes_cbc_decrypt
 from challenge12 import rand_bytes
 from challenge15 import unpad_pkcs7
 
-def build_cookie(user_data, key):
-    user_data = user_data.replace(';', '%3B').replace('=', '%3D')
-    cookie_str = 'comment1=cooking%20MCs;userdata=' + user_data + ';comment2=%20like%20a%20pound%20of%20bacon'
-    return aes_cbc_encrypt(pkcs7_pad(cookie_str), key)
+def cookie_oracle(key):
+    def build_cookie(user_data):
+        user_data = user_data.replace(';', '%3B').replace('=', '%3D')
+        cookie_str = 'comment1=cooking%20MCs;userdata=' + user_data + ';comment2=%20like%20a%20pound%20of%20bacon'
+        return aes_cbc_encrypt(pkcs7_pad(cookie_str), key)
+
+    return build_cookie
 
 def parse_cookie(encrypted_cookie, key):
     cookie = unpad_pkcs7(aes_cbc_decrypt(encrypted_cookie, key))
@@ -19,14 +22,15 @@ def is_admin(encrypted_cookie, key):
     pairs = parse_cookie(encrypted_cookie, key)
     return next((True for k,v in pairs if k=='admin' and v == 'true'), False)
 
-def make_admin(encrypted_cookie):
-    ## Figure out how long hte prepended stuff is and then flip a bit around
-    ## where my equals should be to get it turned on as admin. 
+def build_admin_cookie(oracle):
+    payload = ('A' * 16) + chr(ord(';')-1) + 'admin' + chr(ord('=')-1) + 'true' + chr(ord(';')-1) + ('A' * 4)
+    x = oracle(payload)
+    return x[0:32] + chr(ord(x[32])^1) + x[33:38] + chr(ord(x[38])^1) + x[39:43] + chr(ord(x[43])^1) + x[44:]
 
 if __name__ == '__main__':
     key = rand_bytes()
-    encrypted = build_cookie('admin=true', key)
+    oracle = cookie_oracle(key)
 
+    admin_cookie = build_admin_cookie(oracle)
 
-
-    is_admin(encrypted, key)
+    print 'Is admin: ' + str(is_admin(admin_cookie, key))
