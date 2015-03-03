d34acc14c3500ce07c026015f04d583788175ac7
coffutt <coffutt@paypal.com>
2015-03-03 15:53:34 -0500

    worked out some errs challenge 17 still broken

--- challenge17.py
+++ challenge17.py
@@ -16,9 +16,10 @@ class PaddingOracle():
     def __init__(self):
         self.key = rand_bytes()
         self.iv = rand_bytes()
-        self.lines = self._read_file()
         with open('./data/17.txt') as f:
             self.lines = map(clean_line, f.readlines())
+
+    def get_iv(self): return self.iv
 
     def get_cookie(self):
         padded_line = pkcs7_pad(random.choice(self.lines))
@@ -26,10 +27,14 @@ class PaddingOracle():
 
     def is_valid_padding(self, encrypted_cookie):
         decrypted = aes_cbc_decrypt(encrypted_cookie, self.key, self.iv)
-        return unpad_pkcs7(decrypted) != False
+        try:
+            unpad_pkcs7(decrypted)
+            return True
+        except Exception:
+            return False
 
-def crack_crypto(cipher_text, oracle):
-    blocks = get_blocks(oracle, 16)
+def crack_crypto(cipher_text, oracle, iv):
+    blocks = get_blocks(cipher_text, 16)
     intermediate_blocks = []
     all_chrs = [chr(i) for i in range(256)]
 
@@ -37,15 +42,21 @@ def crack_crypto(cipher_text, oracle):
         intermediate = ''
         for i in range(1, 17):
             for ch in all_chrs:
-                end = ''.join([i ^ c for c in intermediate])
-                if oracle(rand_bytes[16-i] + ch + end):
-                    intermediate = (ch ^ i) + intermediate
+                end = ''.join([chr(i ^ ord(c)) for c in intermediate])
+                if oracle(rand_bytes(16-i) + ch + end):
+                    intermediate = chr((ord(ch) ^ i)) + intermediate
                     break
 
         intermediate_blocks.append(intermediate)
 
+    plain_text = ''
+    for i in range(len(intermediate_blocks)):
+        i_block = intermediate_blocks[i]
+        plain_text += ''.join([chr(ord(p[0]) ^ ord(p[1])) for p in zip(block, iv)])
+        iv = blocks[i]
 
+    return plain_text
 
 if __name__ == '__main__':
     oracle = PaddingOracle()
-    print crack_crypto(oracle.get_cookie(), oracle.is_valid_padding)
+    print crack_crypto(oracle.get_cookie(), oracle.is_valid_padding, oracle.get_iv())
